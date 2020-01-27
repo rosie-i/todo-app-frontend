25a57f99eb078ec30dbdc4e08f341f1cb416c0bf
rosie-i <rosannainman@gmail.com>
2020-01-27 23:53:34 +0000

    addTask functionality with user-inputted description and date working

--- src/NewTaskInput.js
+++ src/NewTaskInput.js
@@ -2,18 +2,39 @@ import React from 'react';
 
 class NewTaskInput extends React.Component {
 
-    addTask = () => {
-        this.props.addTaskFunc("Finish React app", "30/01/2020")
+    state = {
+        newTaskDescription: "No task entered",
+        newTaskDueDate: "No due date entered"
     }
+
+    addTask = () => {
+        this.props.addTaskFunc(this.state.newTaskDescription, this.state.newTaskDueDate)    
+    }
+
+    taskDescriptionChanged = (event) => {
+        this.setState({
+          newTaskDescription: event.target.value
+        });
+    }
+
+
+    // This works but date format is yyyy-mm-dd instead of dd/mm/yyyy
+    // The former is ISO 8601 format, look into changing (with webcomponents or HTML5?)
+    taskDateChanged = (event) => {
+        this.setState({
+          newTaskDueDate: event.target.value
+        });
+    }
+
 
     render() {
         return (
             <div className="row newTask">
                 <div className="col-6 text-right">
-                    <input type="text" name="taskname" placeholder="Enter task here..." />
+                    <input type="text" name="taskname" placeholder="Enter task here..." onChange={this.taskDescriptionChanged} />
                 </div>
                 <div className="col-6 text-left">
-                    <input type="date" />
+                    <input type="date" onChange={this.taskDateChanged} />
                     <button type="button" onClick={this.addTask}>+</button>
                 </div>
             </div>

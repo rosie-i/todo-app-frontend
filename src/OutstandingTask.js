d661d06fda8bab8829fffe3a3b83ea4950309aa3
rosie-i <rosannainman@gmail.com>
2020-01-30 00:02:12 +0000

    Done clicked - task strikethrough and done button does not render

--- src/OutstandingTask.js
+++ src/OutstandingTask.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class OutstandingTask extends React.Component {
-    
+
     deleteClicked = () => {
         this.props.deleteTaskFunc(this.props.item.id)
     }
@@ -9,20 +9,35 @@ class OutstandingTask extends React.Component {
     doneClicked = () => {
         this.props.doneClickedFunc(this.props.item.id)
     }
-    
+
     render() {
+
+
+        // Conditional rendering using variables to store elements
+        // If our task is marked completed: true, this will render with strikethrough text
+
+        let description;
+
+        if (this.props.item.completed) {
+            description = <div className="col-12 col-md-8 gridItemOutstanding completedTaskRendering">{this.props.item.description}</div>
+        } else {
+            description = <div className="col-12 col-md-8 gridItemOutstanding">{this.props.item.description}</div>
+        }
+
+
         return (
             <div className="row gridContainerOutstanding">
-                <div className="col-12 col-md-8 gridItemOutstanding">
-                    {this.props.item.description}
+                {/* This renders the element as defined above using conditional rendering*/}
+                {description}
+
+                <div className="col-6 col-md-2 gridItemOutstanding">
+                    {this.props.item.due}
                 </div>
 
                 <div className="col-6 col-md-2 gridItemOutstanding">
-                {this.props.item.due}
-                </div>
-
-                <div className="col-6 col-md-2 gridItemOutstanding">
+                    {this.props.item.completed === false && (
                     <button type="button" onClick={this.doneClicked}>&#10004;</button>
+                    )}
                     <button type="button" onClick={this.deleteClicked}><b>&#128465;</b></button>
                 </div>
             </div>

82456c4640dcda9ea4caf3f079ddea72a00329f5
rosie-i <rosannainman@gmail.com>
2020-02-26 21:44:00 +0000

    Completed task count fixed

--- src/CompletedTaskCount.js
+++ src/CompletedTaskCount.js
@@ -4,7 +4,7 @@ class CompletedTaskCount extends React.Component {
     render() {
         return (
             <div className="row">
-                <p className="tasksToGo col-12"><b>{this.props.CompletedTaskCount}</b> completed!</p>
+                <p className="tasksToGo col-12"><b>{this.props.completedTaskCount}</b> completed!</p>
             </div>
         );
     }

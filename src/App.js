e6b0691f09485e5156eb38d54a4e07732bc4f36a
rosie-i <rosannainman@gmail.com>
2020-03-04 23:01:09 +0000

    DELETE functioning using axios

--- src/App.js
+++ src/App.js
@@ -20,40 +20,52 @@ class App extends React.Component {
     ]
   }
 
-  
+
   componentDidMount = () => {
     // Fetch tasks from API
     axios.get('https://w8wvzvhojl.execute-api.eu-west-2.amazonaws.com/dev/tasks')
-    .then((response) => {
-      const outstandingTasksFromResponse = response.data.tasks.filter(function(e){
-        return e.completed == false
-      });
-      const completedTasksFromResponse = response.data.tasks.filter(function(e){
-        return e.completed == true
-      });
-      // handle success
-      this.setState({
-        outstandingTasks: outstandingTasksFromResponse,
-        completedTasks: completedTasksFromResponse
+      .then((response) => {
+        const outstandingTasksFromResponse = response.data.tasks.filter(function (e) {
+          return e.completed == false
+        });
+        const completedTasksFromResponse = response.data.tasks.filter(function (e) {
+          return e.completed == true
+        });
+        // handle success
+        this.setState({
+          outstandingTasks: outstandingTasksFromResponse,
+          completedTasks: completedTasksFromResponse
+        })
       })
-    })
-    .catch((error) => {
-      // handle error 
-      console.error(error);
-    });
+      .catch((error) => {
+        // handle error 
+        console.error(error);
+      });
   }
 
 
   deleteTask = (taskID) => {
-    // Tasks will be deleted when this function executes
-    // 1. Get list of tasks from state
+    // Task will be deleted when this function executes
+
+    // Get list of tasks from state
     let tasks = this.state.outstandingTasks;
-    // 2. Identify task that matches the given taskID and remove it
-    let updatedTasks = tasks.filter(item => item.taskID !== taskID);
-    // 3. Update state to reflect deletion with new collection of tasks (i.e. without the one we just deleted)
-    this.setState({
-      outstandingTasks: updatedTasks
-    });
+
+    // Issue post request using axios and pass in the JSON object taskToAdd
+    axios.delete(`https://w8wvzvhojl.execute-api.eu-west-2.amazonaws.com/dev/tasks/${taskID}`)
+      .then((response) => {
+        
+        // Identify task that matches the given taskID and remove it
+        let updatedTasks = tasks.filter(item => item.taskID !== taskID);
+
+        // Update state to reflect deletion with new collection of tasks (i.e. without the one we just deleted)
+        this.setState({
+          outstandingTasks: updatedTasks
+        });
+      })
+      .catch((error) => {
+        // handle error 
+        console.error(error);
+      });
   }
 
   addTask = (newTaskDescription, newTaskDueDate) => {
@@ -64,24 +76,24 @@ class App extends React.Component {
 
     // Issue post request using axios and pass in the JSON object taskToAdd
     axios.post('https://w8wvzvhojl.execute-api.eu-west-2.amazonaws.com/dev/tasks', taskToAdd)
-    .then((response) => {
-      // Tell FE taskID, completed and userID saved from the BE with response.data.tasks so it can be used in state
-      taskToAdd.taskID = response.data.task.taskID;
-      taskToAdd.completed = response.data.task.completed;
-      taskToAdd.userID = response.data.task.userID;
-      // Get current array of tasks from state
-      const tasksToDo = this.state.outstandingTasks;
-      // Add new task object (taskToAdd) to the array
-      tasksToDo.push(taskToAdd);
-      // 4. Update state to reference new array with added task
-      this.setState({
-        outstandingTasks: tasksToDo
+      .then((response) => {
+        // Tell FE taskID, completed and userID saved from the BE with response.data.tasks so it can be used in state
+        taskToAdd.taskID = response.data.task.taskID;
+        taskToAdd.completed = response.data.task.completed;
+        taskToAdd.userID = response.data.task.userID;
+        // Get current array of tasks from state
+        const tasksToDo = this.state.outstandingTasks;
+        // Add new task object (taskToAdd) to the array
+        tasksToDo.push(taskToAdd);
+        // 4. Update state to reference new array with added task
+        this.setState({
+          outstandingTasks: tasksToDo
+        });
+      })
+      .catch((error) => {
+        // handle error 
+        console.error(error);
       });
-    })
-    .catch((error) => {
-      // handle error 
-      console.error(error);
-    });
   }
 
   doneClicked = (taskID) => {
@@ -119,7 +131,7 @@ class App extends React.Component {
     // Push item to array of completed tasks
     const completedTaskList = this.state.completedTasks;
     completedTaskList.push(completedTask);
-    
+
     // Update state 
     this.setState({
       outstandingTasks: outstandingTaskList,
@@ -142,8 +154,8 @@ class App extends React.Component {
             doneClickedFunc={this.doneClicked}
           />
           <CompletedTaskTitle />
-          <CompletedTaskCount completedTaskCount={this.state.completedTasks.length}/>
-          <CompletedTaskList 
+          <CompletedTaskCount completedTaskCount={this.state.completedTasks.length} />
+          <CompletedTaskList
             completedTaskList={this.state.completedTasks}
           />
         </div>

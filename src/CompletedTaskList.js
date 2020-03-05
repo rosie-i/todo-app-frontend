a110eef0bbb04ca2e0690a4c4fbfb08883812092
rosie-i <rosannainman@gmail.com>
2020-03-05 01:01:00 +0000

    Styling changes (task list headers) + new favicon

--- src/CompletedTaskList.js
+++ src/CompletedTaskList.js
@@ -4,7 +4,11 @@ import CompletedTask from './CompletedTask';
 class CompletedTaskList extends React.Component {
     render() {
         return (
-            <div>
+            <div className="taskListBorder">
+                <div className="row gridContainerOutstandingHeader">
+                    <div className="col-md-8 d-md-block d-none">DESCRIPTION</div>
+                    <div className="col-md-4 d-md-block d-none">DUE</div>
+                </div>
                 {
                     this.props.completedTaskList.map(
                         (taskItem) => <CompletedTask
--- src/OutstandingTaskList.js
+++ src/OutstandingTaskList.js
@@ -4,7 +4,12 @@ import OutstandingTask from './OutstandingTask';
 class OutstandingTaskList extends React.Component {
     render() {
         return (
-            <div>
+            <div className="taskListBorder">
+                <div className="row gridContainerOutstandingHeader">
+                    <div className="col-md-8 d-md-block d-none">DESCRIPTION</div>
+                    <div className="col-md-2 d-md-block d-none">DUE</div>
+                    <div className="col-md-2 d-md-block d-none">ACTIONS</div>
+                </div>
                 {
                     this.props.outstandingTaskList.map(
                         (taskItem) => <OutstandingTask
